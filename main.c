c828f7b7fe867758217e29bf29db436b3ddb957c
SangWon Nam <tkddnjsrhd12@naver.com>
2024-03-27 17:28:45 +0900

    feat: main.c 팀이름출력기능추가

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 int main() {
 	char name[10];
 	int num;
+	char t_name[10];
 	printf("이름을 입력하시오: ");
 	scanf_s("%s",name,10);
 	printf("\n");
 	printf("학번을 입력하시오: ");
 	scanf_s("%d", &num);
+	printf("\n");
+	printf("팀 이름을 입력하시오: ");
+	scanf_s("%s",t_name,10);
 	printf("\n");
 
 	printf("<출력>");
@@ -15,5 +19,7 @@ int main() {
 	printf("이름:%s ",name);
 	printf("\n");
 	printf("학번:%d ",num);
+	printf("\n");
+	printf("팀이름:%s",t_name);
 	return 0;
 }
